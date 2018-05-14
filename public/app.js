c95ff7ef9175cb995ceb7fbb11828045908869d7
JLee21 <leejchris21@gamil.com>
2018-05-13 22:42:33 -0700

    done with Controlling Navigation
    Signed-off-by: JLee21 <leejchris21@gamil.com>

--- public/app.js
+++ public/app.js
@@ -1,19 +1,79 @@
-'use strict';
+/***
+ * Excerpted from "Serverless Single Page Apps",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/brapps for more book information.
+***/
+"use strict";
+
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
-  var view= = $('.templates .problem-view').clone();
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = learnjs.template('problem-view');
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      var correctFlash = learnjs.template('correct-flash');
+      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+      learnjs.flashElement(resultFlash, correctFlash);
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
-  return view
+  learnjs.applyObject(problemData, view);
+  return view;
 }
 
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView
   };
-  var hashParts = hash.split('-')
+  var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
-  if (viewFn){
+  if (viewFn) {
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
@@ -23,4 +83,4 @@ learnjs.appOnReady = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
-};
+}
